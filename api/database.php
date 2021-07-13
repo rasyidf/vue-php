1d6347c0391fc8000c1a6c1122c9139767c31054
rasyidf <ufarasfa@gmail.com>
2021-07-13 22:34:09 +0700

    first commit

---
+++ api/database.php
@@ -0,0 +1,19 @@
+<?php
+class Database
+{
+    private $host = "localhost";
+    private $db_name = "db_books";
+    private $username = "root";
+    private $password = "";
+    public $db;
+    public function getConnection()
+    {
+        $this->db = null;
+        try {
+            $this->db = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        } catch (Exception $e) {
+            return false;
+        }
+        return $this->db;
+    }
+}
